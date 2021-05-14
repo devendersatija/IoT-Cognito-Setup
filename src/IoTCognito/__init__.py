d5c18781468c242b827b9318aadb0f083a641fb5
Devender Satija <satijads@amazon.com>
2021-05-14 17:33:07 -0400

    restructuring

---
+++ src/IoTCognito/__init__.py
@@ -0,0 +1,5 @@
+from .attach_iot_policy import attach_iot_policy
+from .get_auth_creds import get_auth_credentials
+from .get_unauth_creds import get_unauth_credentials
+from .get_user_input import get_user_input
+from .publish_mqtt import publish_mqtt
